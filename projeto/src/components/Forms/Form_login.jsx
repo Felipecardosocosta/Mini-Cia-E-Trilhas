f85f4f36cb9d48a2cef1f4f58c8368484d0a3f6a
Felipecardosocosta <felipe.cardoso.costa96@gmail.com>
2025-12-02 06:36:33 -0300

    Merge branch 'main' of https://github.com/Felipecardosocosta/Mini-Cia-E-Trilhas

--- projeto/src/components/Forms/Form_login.jsx
+++ projeto/src/components/Forms/Form_login.jsx
@@ -22,14 +22,15 @@ function Form_login() {
         if (resposta.status === 200) {
             Swal.fire({
                 position: "top-end",
-                icon:"success",
                 title: "Login realizado com sucesso!",
                 showConfirmButton: false,
-                timer: 2000,
-                customClass:{
-                    popup: 'alerta'
+                timer: 200000,
+                iconColor:'rgba(0, 0, 0, 1)',
+                customClass: {
+                    popup: 'alerta',
+
                 }
-                
+
             });
             setUser(resposta.data.result)
             setModalLogin(false)
